b45b90259357bcf23b54d98ea27c29372e79fc7f
Nader youb <youb.nader@gmail.com>
2025-07-24 16:21:23 +0100

    feat: add footer component for admin view

---
+++ src/app/component/footer.tsx
@@ -0,0 +1,12 @@
+//gimme a footer for the admin view
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer className="text-center text-glow">
+      Nexus Club © 2025
+    </footer>
+  );
+};
+
+export default Footer;
