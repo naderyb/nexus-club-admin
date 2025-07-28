465009a1ee60e7208429003fdeb6b65d4416bdc2
Nader youb <youb.nader@gmail.com>
2025-07-28 13:40:47 +0100

    feat: integrate Vercel Speed Insights and Analytics for enhanced performance tracking

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,8 @@
 import { Orbitron } from "next/font/google";
 import "./globals.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
+import { Analytics } from "@vercel/analytics/next";
 const orbitron = Orbitron({ subsets: ["latin"], weight: "600" });
-
 
 export default function Layout({ children }: { children: React.ReactNode }) {
   return (
@@ -12,11 +13,13 @@ export default function Layout({ children }: { children: React.ReactNode }) {
         <link rel="icon" href="/favicon.ico" />
         <link rel="preconnect" href="https://fonts.googleapis.com" />
       </head>
-    <body className={`${orbitron.className} bg-background text-white`}>
-      <div className="min-h-screen bg-background text-white flex flex-col">
-        <main className="flex-1 p-6">{children}</main>
-      </div>
-    </body>
+      <body className={`${orbitron.className} bg-background text-white`}>
+        <div className="min-h-screen bg-background text-white flex flex-col">
+          <main className="flex-1 p-6">{children}</main>
+        </div>
+        <SpeedInsights />
+        <Analytics />
+      </body>
     </html>
   );
 }
